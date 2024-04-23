7f07a18a9c7c886687dffa988096ff9b56cfc2a2
yalikes <algebnaly@qq.com>
2024-04-23 20:03:58 +0800

    change output path to Plugins

--- Config.lua
+++ Config.lua
@@ -1,30 +1,33 @@
 return {
-    Title = "藏书阁增强",
-    Author = "在下炮灰",
-    Cover = "Cover.png",
-    WorkshopCover = "Cover.png",
-    Source = 1,
-    TagList = {
-		"数值改动"
-    },
-    FrontendPlugins = {
-	    "BookRepairEnhanceFrontend.dll"
-    },
-    BackendPlugins = {
-        "BookRepairEnhanceBackend.dll"
-    },
-    Description = [[
-        简单地缩短藏书阁修书所需要的时间
-        修书所需的点数由下九品到神一品变化如下
-        50   -> 25 
-        100  -> 50
-        200  -> 100
-        400  -> 150
-        800  -> 200
-        1600 -> 300
-        2400 -> 500
-        3200 -> 750
-        4000 -> 1000
-        Github: https://github.com/yalikes/TaiwuBookRepairEnhance
-        ]]
+	Title = "藏书阁增强",
+	Author = "在下炮灰",
+	Cover = "Cover.png",
+	WorkshopCover = "Cover.png",
+	Source = 0,
+	TagList = {
+		[1] = "Modifications",
+	},
+	FrontendPlugins = {
+		[1] = "TaiwuBookRepairEnhanceFrontend.dll",
+	},
+	BackendPlugins = {
+		[1] = "TaiwuBookRepairEnhanceBackend.dll",
+	},
+	FileId = 2894638868,
+	Description = "        简单地缩短藏书阁修书所需要的时间\n        修书所需的点数由下九品到神一品变化如下\n        50   -> 25 \n        100  -> 50\n        200  -> 100\n        400  -> 150\n        800  -> 200\n        1600 -> 300\n        2400 -> 500\n        3200 -> 750\n        4000 -> 1000\n        Github: TaiwuBookRepairEnhance\n        ",
+	Version = "1.0.0.0",
+	GameVersion = "0.0.70.51",
+	Visibility = 0,
+	DefaultSettings = { },
+	UpdateLogList = {
+		[1] = {
+			Timestamp = 1713873412,
+			LogList = {
+				[1] = "配适新版",
+			},
+		},
+	},
+	ChangeConfig = false,
+	HasArchive = false,
+	NeedRestartWhenSettingChanged = false,
 }
